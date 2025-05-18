3c3a774bec93303396fb1889d28921ffd8a53687
AQuan05 <trandihanhquan17072021@gmail.com>
2025-05-18 08:00:25 +0700

    database chua hoan thien

---
+++ HN_670/database/migrations/2025_05_17_032929_create_product_variations_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductVariationsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('color_id')->constrained()->onDelete('cascade');
+            $table->foreignId('size_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity')->default(0);
+            $table->decimal('price', 12, 2);
+            $table->decimal('price_sale', 12, 2)->nullable();
+            $table->boolean('status')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('product_variations');
+    }
+}
