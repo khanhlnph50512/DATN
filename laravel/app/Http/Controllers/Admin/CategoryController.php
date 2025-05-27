344e0413c99cd792bfbde99163ec047df1e2eb8c
AQuan05 <trandihanhquan17072021@gmail.com>
2025-05-27 22:02:11 +0700

    crud categories (done)

---
+++ laravel/app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Models\Admin\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.index', compact('categories'));
+    }
+    // public function getData()
+    // {
+    //     return DataTables::of(Category::query())->make(true);
+    // }
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'  => 'required|string|max:255',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $imagePath = null;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('asset/img/category'), $imageName);
+            $imagePath = 'asset/img/category/' . $imageName;
+        }
+
+        Category::create([
+            'name'  => $request->name,
+            'slug'  => Str::slug($request->name),
+            'image' => $imagePath,
+        ]);
+
+        return redirect()->route('admin.categories.index')->with('success', 'Danh mục đã được tạo!');
+    }
+    public function edit(Category $category)
+    {
+        return view('admin.categories.edit', compact('category'));
+    }
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name'  => 'required|string|max:255',
+            'image' => 'nullable|image|max:2048',
+        ]);
+
+        $data = [
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ];
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('asset/img/category'), $imageName);
+            $data['image'] = 'asset/img/category/' . $imageName;
+        }
+
+        $category->update($data);
+
+        return redirect()->route('admin.categories.index')->with('success', 'Cập nhật thành công!');
+    }
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->route('admin.categories.index')->with('success', 'Category deleted.');
+    }
+}
